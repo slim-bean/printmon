040b99d22fc02b3cda4775ac6509d0dfde9a528c
Ed Welch <ed@edjusted.com>
2022-05-17 12:01:28 -0400

    add config files

---
+++ include/config.h
@@ -0,0 +1,12 @@
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+
+#define URL "loki.edjusted.com" // "192.168.0.1" or "lokiserver.yourdomain.local" No http or https, No port or path
+#define PORT 443
+#define PATH "/loki/api/v1/push"
+#define NTP "172.20.31.1"
+
+#define PROM_URL "cortex.edjusted.com"
+#define PROM_PATH "/api/v1/push"
+
+#define OTA_PASS ""
